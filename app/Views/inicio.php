d4e31ef2b6548def4f213f82946d05786b489990
Antonio Coronel <antoniocoronel368@mail.com>
2025-04-02 16:34:12 -0300

    detalles

--- app/Views/inicio.php
+++ app/Views/inicio.php
@@ -1,7 +1,41 @@
 <div class="margin-text">
-  <img width="100%"
-    src="<?php echo base_url("img/25SS_Ecom_BR_Brand-Campaign-Running_Week-1_Homepage_Full-Bleed-Hero_Large-Desk_2000x694px.jpg") ?>"
-    alt="">
+  <div id="carouselExampleIndicators" class="carousel slide">
+    <div class="carousel-indicators">
+      <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="0" class="active"
+        aria-current="true" aria-label="Slide 1"></button>
+      <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="1"
+        aria-label="Slide 2"></button>
+      <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="2"
+        aria-label="Slide 3"></button>
+    </div>
+    <div class="carousel-inner">
+      <div class="carousel-item active">
+        <img width="100%"
+          src="<?php echo base_url("img/25SS_Ecom_BR_Brand-Campaign-Running_Week-1_Homepage_Full-Bleed-Hero_Large-Desk_2000x694px.jpg") ?>"
+          alt="">
+      </div>
+      <div class="carousel-item">
+        <img width="100%"
+          src="<?php echo base_url("img/25SS_Ecom_BR_Brand-Campaign-Running_Week-2_Homepage_Full-Bleed-Hero_Large-Desk_2000x694px (1).jpg") ?>"
+          alt="">
+      </div>
+      <div class="carousel-item">
+        <img width="100%"
+          src="<?php echo base_url("img/25SS_Ecom_BR_Brand-Campaign-Running_Week-2_Homepage_Full-Bleed-Hero_Large-Desk_2000x694px (1).jpg") ?>"
+          alt="">
+      </div>
+    </div>
+    <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleIndicators"
+      data-bs-slide="prev">
+      <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+      <span class="visually-hidden">Previous</span>
+    </button>
+    <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleIndicators"
+      data-bs-slide="next">
+      <span class="carousel-control-next-icon" aria-hidden="true"></span>
+      <span class="visually-hidden">Next</span>
+    </button>
+  </div>
 </div>
 
 <h2 class="text-center mt-5 mb-5 display-6 fw-bold">Zapatillas para cada ocasión, comodidad y diseño a tu alcance >>>
